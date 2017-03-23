a2e794daf43591e440994a88cfe5ff3c6d7f3ff4
sunsun <bigsun1992@sina.cn>
2017-03-23 14:36:26 +0800

    source address

--- express_demo.js
+++ express_demo.js
@@ -1,6 +1,7 @@
 'use strict'
 
 //Express 是一个简洁而灵活的 node.js Web应用框架, 提供了一系列强大特性帮助你创建各种 Web 应用，和丰富的 HTTP 工具。
+// http://www.runoob.com/nodejs/nodejs-express-framework.html
 
 var express = require('express');
 var app = express();
